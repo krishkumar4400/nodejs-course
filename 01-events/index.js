f294af20c5695f0ca28257eaac161df04a21310d
krishkumar4400 <krishkumarbgp2022@gmail.com>
2026-02-09 13:54:55 +0530

    chat app completed using event emitters

---
+++ 01-events/index.js
@@ -0,0 +1,24 @@
+const ChatRoom = require('./chatRoom.js');
+
+const chat = new ChatRoom();
+
+chat.on('join', (user) => {
+    console.log(`${user} has joined the chat`);
+});
+chat.on('message', (user, message) => {
+    console.log(`${user} : ${message}`);
+});
+chat.on('leave', (user) => {
+    console.log(`${user} has left the chat`);
+});
+
+// simulating the chat
+chat.join('Alice');
+chat.join('bob');
+
+chat.sendMessage('Alice', 'hey bob, Hello to everyone');
+chat.sendMessage('bob', 'hey Alice, Hello to everyone');
+
+chat.leave('Alice');
+chat.sendMessage('Alice', "This message won't be sent");
+chat.leave('bob');
