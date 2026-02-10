17b582d8efce082d2aac7e56d3c30c92fd4ea2d8
krishkumar4400 <krishkumarbgp2022@gmail.com>
2026-02-10 18:01:51 +0530

    Creating and Integrating Custom Modules

---
+++ module-import-export/index.js
@@ -0,0 +1,16 @@
+const value = require('./lib/math.js');
+console.log(value.addFunction(2,5)); // 7
+
+// destructuring directly
+const {addFunction, subtractFunction, multiply, division} = require('./lib/math.js');
+
+console.log(addFunction(1,2)); // 3
+
+console.log(value.subtractFunction(23,3)); // 20
+console.log(value.multiply(3,5)); // 15
+console.log(value.division(15,3)); // 5
+
+console.log(addFunction(1,22));
+console.log(subtractFunction(1,22));
+console.log(multiply(1,22));
+console.log(division(1,22));
---
+++ module-import-export/lib/math.js
@@ -0,0 +1,16 @@
+// Named export
+
+exports.addFunction = function add(a, b) {
+  return a + b;
+};
+
+
+exports.subtractFunction = function sub(a, b) {
+  return a - b;
+}
+exports.multiply = function mul(a, b) {
+  return a * b;
+}
+exports.division = function div(a, b) {
+  return a / b;
+}
