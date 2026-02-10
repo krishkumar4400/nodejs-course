93c0c13fbbbb959a9a2504015a9763be1e936cad
krishkumar4400 <krishkumarbgp2022@gmail.com>
2026-02-10 18:26:08 +0530

    Creating and Integrating Custom Modules

--- module-import-export/index.js
+++ module-import-export/index.js
@@ -1,16 +1,20 @@
-const value = require('./lib/math.js');
-console.log(value.addFunction(2,5)); // 7
+// const value = require('./lib/math.js');
+// console.log(value.addFunction(5,2)); // 7
 
-// destructuring directly
-const {addFunction, subtractFunction, multiply, division} = require('./lib/math.js');
+// // destructuring directly
+// const {addFunction, subtractFunction, multiply, division} = require('./lib/math.js');
 
-console.log(addFunction(1,2)); // 3
+// console.log(addFunction(1,2)); // 3
 
-console.log(value.subtractFunction(23,3)); // 20
-console.log(value.multiply(3,5)); // 15
-console.log(value.division(15,3)); // 5
+// console.log(value.subtractFunction(23,3)); // 20
+// console.log(value.multiply(3,5)); // 15
+// console.log(value.division(15,3)); // 5
 
-console.log(addFunction(1,22));
-console.log(subtractFunction(1,22));
-console.log(multiply(1,22));
-console.log(division(1,22));
+// console.log(addFunction(1,22));
+// console.log(subtractFunction(1,22));
+// console.log(multiply(1,22));
+// console.log(division(1,22));
+
+// importing for default export
+const xyz = require('./lib/math.js');
+xyz();
--- module-import-export/lib/math.js
+++ module-import-export/lib/math.js
@@ -1,16 +1,26 @@
-// Named export
+// // Named export
 
-exports.addFunction = function add(a, b) {
-  return a + b;
-};
+// exports.addFunction = function add(a, b) {
+//   return a + b;
+// };
 
 
-exports.subtractFunction = function sub(a, b) {
-  return a - b;
+// exports.subtractFunction = function sub(a, b) {
+//   return a - b;
+// }
+// exports.multiply = function mul(a, b) {
+//   return a * b;
+// }
+// exports.division = function div(a, b) {
+//   return a / b;
+// }
+
+
+// const {xyz} = require('../test/a/b/test.js');
+// console.log(xyz);
+
+// default export
+module.exports = function() {
+  console.log("Hey, I am default");
 }
-exports.multiply = function mul(a, b) {
-  return a * b;
-}
-exports.division = function div(a, b) {
-  return a / b;
-}
+
