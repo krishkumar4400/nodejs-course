af5374ce23e596f8f8a0cbaff65cc6373fb6217e
krishkumar4400 <krishkumarbgp2022@gmail.com>
2026-02-13 21:37:23 +0530

    implemented session based authentication

--- authentication-session/index.js
+++ authentication-session/index.js
@@ -1,10 +1,10 @@
-import express from 'express';
-import userRouter from './routes/user.routes.js';
-import adminRouter from './routes/admin.routes.js';
+import express from "express";
+import userRouter from "./routes/user.routes.js";
+import adminRouter from "./routes/admin.routes.js";
 
-import { authenticationMiddleware } from './middlewares/auth.middleware.js';
+import { authenticationMiddleware } from "./middlewares/auth.middleware.js";
 
-import jwt from 'jsonwebtoken';
+import jwt from "jsonwebtoken";
 
 const app = express();
 const PORT = process.env.PORT ?? 8000;
@@ -12,11 +12,11 @@ const PORT = process.env.PORT ?? 8000;
 app.use(express.json());
 app.use(authenticationMiddleware);
 
-app.get('/', (req, res) => {
-  return res.json({ status: 'Server is up and running' });
+app.get("/", (req, res) => {
+  return res.json({ status: "Server is up and running" });
 });
 
-app.use('/user', userRouter);
-app.use('/admin', adminRouter);
+app.use("/user", userRouter);
+app.use("/admin", adminRouter);
 
 app.listen(PORT, () => console.log(`Server is running on PORT: ${PORT}`));
