89c32eaa8feab9f3bf8c268c2f37f4a3dbfb67e4
krishkumar4400 <krishkumarbgp2022@gmail.com>
2026-02-22 14:46:07 +0530

    implement session based authentication

--- projects/02-auth-system/routes/user.js
+++ projects/02-auth-system/routes/user.js
@@ -1,12 +1,18 @@
 import { Router } from "express";
-import { getAllUser, getUserData, login, signUp, updateUser } from "../controllers/user.js";
+import {
+  getAllUser,
+  getUserData,
+  login,
+  signUp,
+  updateUser,
+} from "../controllers/user.js";
 
 const userRouter = Router();
 
-userRouter.get('/', getAllUser);
-userRouter.get('/me', getUserData);
-userRouter.patch('/update', updateUser);
-userRouter.post('/sign-up', signUp);
-userRouter.post('/login', login);
+userRouter.get("/", getAllUser);
+userRouter.get("/me", getUserData);
+userRouter.patch("/update", updateUser);
+userRouter.post("/sign-up", signUp);
+userRouter.post("/login", login);
 
 export default userRouter;
